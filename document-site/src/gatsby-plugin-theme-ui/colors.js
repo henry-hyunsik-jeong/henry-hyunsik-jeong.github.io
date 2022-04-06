52b9d39216bda621493be05b307ab95264336674
henry91-jung <henry91.jung@samsung.com>
2022-04-06 14:56:59 +0000

    initial commit after install template and test revision

---
+++ document-site/src/gatsby-plugin-theme-ui/colors.js
@@ -0,0 +1,35 @@
+export default {
+  text: "#000",
+  background: "#fff",
+  primary: "#e63b19",
+  secondary: "#c70d3a",
+  sidebar: "#eee",
+  borderColor: "rgba(0, 0, 0, 0.15)",
+
+  modes: {
+    dark: {
+      text: '#fff',
+      background: 'hsl(230,25%,18%)',
+      primary: 'hsl(260, 100%, 80%)',
+      secondary: 'hsl(290, 100%, 80%)',
+      sidebar: 'hsla(230, 20%, 0%, 20%)',
+      borderColor: 'rgba(255, 255, 255, 0.15)'
+    },
+    cool: {
+      text: "#000",
+      background: "#fff",
+      primary: "#e63b19",
+      secondary: "#c70d3a",
+      sidebar: "#eee",
+      borderColor: "rgba(0, 0, 0, 0.15)"
+    },
+    deep: {
+      text: '#fff',
+      background: 'hsl(230,25%,18%)',
+      primary: 'hsl(260, 100%, 80%)',
+      secondary: 'hsl(290, 100%, 80%)',
+      sidebar: 'hsla(230, 20%, 0%, 20%)',
+      borderColor: 'rgba(255, 255, 255, 0.15)'
+    }
+  }
+};
