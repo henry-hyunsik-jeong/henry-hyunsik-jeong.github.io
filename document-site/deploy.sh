bf396f79c33ea92a2bc4c6f609abf67f6870873b
henry-hyunsik-jeong <henry-hyunsik-jeong@gmail.com>
2022-04-23 14:50:26 +0900

    commit_220423

--- document-site/deploy.sh
+++ document-site/deploy.sh
@@ -2,7 +2,6 @@ RELATIVE_DIR=`dirname "$BASH_SOURCE"`
 CANONICAL_DIR=`readlink -f $RELATIVE_DIR`
 CUR_DIR=${CANONICAL_DIR}
 
-
-gatsby build --prefix-paths
 python ${CUR_DIR}/python_code_to_run_before_deploy/run_before_deploy.py
+gatsby build --prefix-paths
 npm run deploy
